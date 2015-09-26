ef833b8698979110ce923b2dba94351f970a690e
markchennai <mahesh.rajakumar@gmail.com>
2015-09-26 06:58:37 +0530

    adding BSTNode class
    implementing Node class for a bst operation

--- test.cpp
+++ test.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+class bstNode
+{
+	private:
+	bstNode *leftPtr;
+	int data;
+	bstNode *rightPtr;
+	
+	public:
+	bstNode(int d):leftPtr(nullptr), data(d), rightPtr(nullptr)
+	{}
+};
+
 int main()
 {
 	cout << endl << "Jesus Loves You !!!";
